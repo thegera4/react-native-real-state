e5b924bea8c59e22cf8a3ca273cb0d415a156271
thegera4 <84020433+thegera4@users.noreply.github.com>
2025-01-29 18:51:10 -0600

    update: added data fetching

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,29 +1,61 @@
-import {Button, FlatList, Image, SafeAreaView, Text, TouchableOpacity, View} from 'react-native'
+import {useEffect} from "react"
+import {FlatList, Image, SafeAreaView, Text, TouchableOpacity, View} from 'react-native'
 import icons from "@/constants/icons"
 import Search from "@/components/Search"
 import {Card, FeaturedCard} from "@/components/Cards"
 import Filters from "@/components/Filters"
 import {useGlobalContext} from "@/lib/global-provider"
-import seed from "@/lib/seed"
+import {router, useLocalSearchParams} from "expo-router"
+import {useAppwrite} from "@/lib/useAppwrite"
+import {getLatestProperties, getProperties} from "@/lib/appwrite"
 
 export default function Index() {
   const { user } = useGlobalContext()
   const uri: string | undefined = user?.avatar.toString()
   const userName: string | undefined = user?.name
 
+  const params = useLocalSearchParams<{ query?: string, filter?: string; }>()
+
+  const { data: latestProperties, loading: latestPropertiesLoading } = useAppwrite({ fn: getLatestProperties, })
+
+  const { data: properties, loading, refetch } = useAppwrite({
+    fn: getProperties,
+    params: {
+      filter: params.filter!,
+      query: params.query!,
+      limit: 6
+    },
+    skip: true,
+  })
+
+  // Refetch properties when the filter or query changes
+  useEffect(() => {
+    refetch({
+      filter: params.filter!,
+      query: params.query!,
+      limit: 6
+    })
+  }, [params.filter, params.query])
+
+  /**
+   * Function to handle the card press and navigate to the property details screen.
+   * @param {string} id - The property id.
+   */
+  const handleCardPress = (id: string) => router.push(`/properties/${id}`)
+
   return (
     <SafeAreaView className="bg-white h-full">
-      <Button onPress={()=>seed()} title="Press me" />
       <FlatList
-        data={[1, 2, 3, 4]}
-        renderItem={({ item }) => <Card onPress={() => console.log("Card pressed")} /> }
+        data={properties}
+        renderItem={({item}) => <Card item={item} onPress={() => handleCardPress(item.$id)}/>}
         keyExtractor={(item) => item.toString()}
         numColumns={2}
         contentContainerClassName="pb-32"
-        columnWrapperClassName="flex gap-5 px-5"showsVerticalScrollIndicator={false}
+        columnWrapperClassName="flex gap-5 px-5"
+        showsVerticalScrollIndicator={false}
         ListHeaderComponent={
           <View className="px-5">
-
+            {/* Top bar (avatar, name and notifications icon) */}
             <View className="flex flex-row items-center justify-between mt-5">
               <View className="flex flex-row items-center">
                 <Image source={{uri}} className="size-12 rounded-full" />
@@ -34,9 +66,9 @@ export default function Index() {
               </View>
               <Image source={icons.bell} className="size-6" />
             </View>
-
+            {/* Search bar */}
             <Search />
-
+            {/* Featured and see all texts */}
             <View className="my-5">
               <View className="flex flex-row items-center justify-between">
                 <Text className="text-xl font-rubik-bold text-black-300">Destacadas</Text>
@@ -44,25 +76,25 @@ export default function Index() {
                   <Text className="text-base font-rubik-bold text-blue-600">Ver todo</Text>
                 </TouchableOpacity>
               </View>
-
+              {/* Featured cards */}
               <FlatList
                 bounces={false}
-                data={[5, 6, 7]}
+                data={latestProperties}
                 keyExtractor={(item) => item.toString()}
                 horizontal={true}
                 showsHorizontalScrollIndicator={false}
                 contentContainerClassName="flex gap-5 mt-5"
-                renderItem={() => <FeaturedCard onPress={() => console.log("Featured card pressed")} />}
+                renderItem={({item}) => <FeaturedCard item={item} onPress={() => handleCardPress(item.$id)} />}
               />
             </View>
-
+            {/* Recommended and see all texts */}
             <View className="flex flex-row items-center justify-between">
               <Text className="text-xl font-rubik-bold text-black-300">Recomendadas</Text>
               <TouchableOpacity>
                 <Text className="text-base font-rubik-bold text-blue-600">Ver todo</Text>
               </TouchableOpacity>
             </View>
-
+            {/* Filters */}
             <Filters />
           </View>
         }
--- components/Cards.tsx
+++ components/Cards.tsx
@@ -2,27 +2,29 @@ import {View, Text, Image, Pressable} from 'react-native'
 import React from 'react'
 import images from "@/constants/images"
 import icons from "@/constants/icons"
+import {Models} from "react-native-appwrite"
 
 /** Card component props for both types of cards. */
 interface CardProps {
+  item: Models.Document
   onPress: () => void
 }
 
 /** Featured card component (full image background). */
-export const FeaturedCard = ({ onPress } : CardProps) => {
+export const FeaturedCard = ({ item, onPress } : CardProps) => {
   return (
     <Pressable onPress={onPress} className="flex flex-col items-start w-60 h-80 relative">
-      <Image source={images.japan} className="size-full rounded-2xl" />
+      <Image source={{ uri: item.image}} className="size-full rounded-2xl" />
       <Image source={images.cardGradient} className="size-full rounded-2xl absolute bottom-0" />
       <View className="flex flex-row items-center bg-white/90 px-3 py-1.5 rounded-full absolute top-5 right-5">
         <Image source={icons.star} className="size-3.5" />
-        <Text className="text-xs font-rubik-bold text-primary-100 ml-1">4.5</Text>
+        <Text className="text-xs font-rubik-bold text-primary-100 ml-1">{item.rating}</Text>
       </View>
       <View className="flex flex-col items-start absolute bottom-5 inset-x-5">
-        <Text className="text-xl font-rubik-extrabold text-white" numberOfLines={1}>Casa completa</Text>
-        <Text className="text-base font-rubik text-white">La Esperanza #277</Text>
+        <Text className="text-xl font-rubik-extrabold text-white" numberOfLines={1}>{item.name}</Text>
+        <Text className="text-base font-rubik text-white">{item.address}</Text>
         <View className="flex flex-row items-center justify-between w-full">
-          <Text className="text-xl font-rubik-extrabold text-white">$1,000</Text>
+          <Text className="text-xl font-rubik-extrabold text-white">${item.price}</Text>
         </View>
       </View>
     </Pressable>
@@ -30,19 +32,19 @@ export const FeaturedCard = ({ onPress } : CardProps) => {
 }
 
 /** Smaller card component */
-export const Card = ({ onPress } : CardProps) => {
+export const Card = ({ item, onPress } : CardProps) => {
   return (
     <Pressable onPress={onPress} className="flex-1 w-full mt-4 px-3 py-4 rounded-lg bg-white shadow-lg shadow-black-100/70 relative">
       <View className="flex flex-row items-center absolute px-2 top-5 right-5 bg-white/90 p-1 rounded-full z-50">
         <Image source={icons.star} className="size-2.5" />
-        <Text className="text-xs font-rubik-bold text-primary-100 ml-0.5">4.5</Text>
+        <Text className="text-xs font-rubik-bold text-primary-100 ml-0.5">{item.rating}</Text>
       </View>
-      <Image source={images.newYork} className="w-full h-40 rounded-lg" />
+      <Image source={{ uri: item.image}} className="w-full h-40 rounded-lg" />
       <View className="flex flex-col mt-2">
-        <Text className="text-base font-rubik-bold text-black-300">Casa funcional</Text>
-        <Text className="text-xs font-rubik text-black-200">La Esperanza #209</Text>
+        <Text className="text-base font-rubik-bold text-black-300">{item.name}</Text>
+        <Text className="text-xs font-rubik text-black-200">{item.address}</Text>
         <View className="flex flex-row items-center justify-between mt-2">
-          <Text className="text-base font-rubik-bold text-black-300">$900</Text>
+          <Text className="text-base font-rubik-bold text-black-300">${item.price}</Text>
         </View>
       </View>
     </Pressable>
